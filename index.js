545ceefe380d30bcc1ed069d607556a663e4518e
Akhilendu <akhilendu.chaturvedi@informa.com>
2023-02-28 19:30:12 +0530

    cors fixed

--- index.js
+++ index.js
@@ -7,62 +7,46 @@ const multer = require("multer")
 // const redis = require('./redis');
 
 const routes = require('./src/routes/routes');
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const port = process.env.PORT || 4000;
-
-app.get('/', (req, res) => {
-    res.send("Welcome");
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 });
 
 app.get('/route', async (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    console.log("ip",ip)
-    // const requests = await incr(ip);
-    // console.log("request", requests)
-    // console.log(`Number of requests made so far {requests}`);
-    // if (requests === 1) {
-    //     await redis.expire(ip, 60);
-    // }
-    // if (requests > 5) {
-    //     res.status(503)
-    //         .json({
-    //             response: 'Error',
-    //             callsMade: requests,
-    //             msg: 'Too many calls made'
-    //         });
-    // } else
-        res.json('You have successfully hit route');
+  res.json('You have successfully hit route');
 })
 
-// app.use(limiter);
-
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads/")
-    },
-    filename: (req, file, cb) => {
-      cb(null, Date.now() + "-" + file.originalname)
-    },
-  })
-  
-  const uploadStorage = multer({ storage: storage })
-  
-  // Single file
-  app.post("/upload/single", uploadStorage.single("file"), (req, res) => {
-    console.log(req.file)
-    return res.send("Single file")
-  })
-  //Multiple files - but here uploaded files limited to 10
-  app.post("/upload/multiple", uploadStorage.array("file", 10), (req, res) => {
-    console.log(req.files)
-    return res.send("Multiple files")
-  })
+  destination: (req, file, cb) => {
+    cb(null, "uploads/")
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname)
+  },
+})
+
+const uploadStorage = multer({ storage: storage })
+
+// Single file
+app.post("/upload/single", uploadStorage.single("file"), (req, res) => {
+  console.log(req.file)
+  return res.send("Single file")
+})
+//Multiple files - but here uploaded files limited to 10
+app.post("/upload/multiple", uploadStorage.array("file", 10), (req, res) => {
+  console.log(req.files)
+  return res.send("Multiple files")
+})
 
 routes.apiRoutes(app);
 
-app.listen(port, function () {
-    console.log(`Node server is listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Node server is listening on port ${port}`);
 });
